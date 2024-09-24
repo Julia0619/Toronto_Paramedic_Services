1043e29157022a9d668a12f82c9787463b5da4af
Julia Lee <jlee.lee@mail.utoronto.ca>
2024-09-24 00:43:04 -0400

    Successfully downloaded the data and wrote code to save it. Saved the original xlsx file in separate csv files by year. The raw data is organized by year in the "data/raw_data" folder.

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -28,8 +28,68 @@ data <- filter(datastore_resources, row_number()==1) %>%
 
 data
 
-#### Saving the Data ####
-# Save the downloaded data as a csv file in data/raw_data
-# The csv file will be called "raw_data.csv"
+#### Creating a Dataframe with the Downloaded Data ####
 
-write_xl(data, "data/raw_data/raw_data.xlsx") 
+# As the downloaded data is in "xlsx" format and very large, a dataframe for each year's 
+# raw data will be created and saved as separate csv files
+# Each dataframe will be saved in the "data/raw_data" folder
+
+# Dataframe for 2017 Data
+
+paramedic_services_data_2017 <- 
+  tibble(
+    data$"2017")
+
+paramedic_services_data_2017
+
+write.csv(paramedic_services_data_2017, file = "data/raw_data/raw_data_2017.csv")
+
+# Dataframe for 2018 Data
+
+paramedic_services_data_2018 <- 
+  tibble(
+    data$"2018")
+
+paramedic_services_data_2018
+
+write.csv(paramedic_services_data_2018, file = "data/raw_data/raw_data_2018.csv")
+
+# Dataframe for 2019 Data
+
+paramedic_services_data_2019 <- 
+  tibble(
+    data$"2019")
+
+paramedic_services_data_2019
+
+write.csv(paramedic_services_data_2019, file = "data/raw_data/raw_data_2019.csv")
+
+# Dataframe for 2020 Data
+
+paramedic_services_data_2020 <- 
+  tibble(
+    data$"2020")
+
+paramedic_services_data_2020
+
+write.csv(paramedic_services_data_2020, file = "data/raw_data/raw_data_2020.csv")
+
+# Dataframe for 2021 Data
+
+paramedic_services_data_2021 <- 
+  tibble(
+    data$"2021")
+
+paramedic_services_data_2021
+
+write.csv(paramedic_services_data_2021, file = "data/raw_data/raw_data_2021.csv")
+
+# Dataframe for 2022 Data
+
+paramedic_services_data_2022 <- 
+  tibble(
+    data$"2022")
+
+paramedic_services_data_2022
+
+write.csv(paramedic_services_data_2022, file = "data/raw_data/raw_data_2022.csv")
